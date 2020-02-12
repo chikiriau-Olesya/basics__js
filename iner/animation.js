51d3ec02ebc1be27b4038b8751ed9bd468d3473f
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-02-12 23:46:48 +0300

    новое

--- iner/animation.js
+++ iner/animation.js
@@ -10,12 +10,26 @@ let box = document.querySelector(".box")
 function paintItBlack() {
   box.style.backgroundColor = "black"
 }
+
 function makeCurcle() {
   box.style.borderRadius = "50%"
 }
+
 function move() {
   box.style.margin = "100px 100px"
 }
+
+
 function makeBlinking() {
-  box.style.opacity = "50%"
+  box.classList.add('blinking')
+}
+
+function addText() {
+  box.before('TITLE', document.createElement('h1'))
+  /*
+  var title = document.createElement('h1')
+  title.innerHTML = "TITLE"
+  title.className = 'title'
+  div.before(title)
+  */
 }
