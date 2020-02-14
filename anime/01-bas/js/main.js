96282414a13b100be1d1f54a67aebd314c0eaf7a
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-02-14 18:02:34 +0300

    add

--- anime/01-bas/js/main.js
+++ anime/01-bas/js/main.js
@@ -25,3 +25,39 @@ let propsAnimation01 = anime({
 })
 let propsBtn01 = document.getElementById('propsBtn01')
 propsBtn01.onclick = propsAnimation01.play
+
+let valsAnimation01 = anime({
+  targets: '.vals .triangle',
+  translateX: 500,
+  easing: 'easeInOutQuint',
+  autoplay: false,
+  delay: anime.stagger(100),
+  rotate: anime.stagger([-270, 270])
+})
+let valsBtn01 = document.getElementById('valsBtn01')
+valsBtn01.onclick = valsAnimation01.play
+
+let path = anime.path('path')
+let svgMotion = anime({
+  targets: '.svgAnim .triangle',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 100000,
+  loop: true
+})
+
+let funcAnim01 = anime({
+  targets: '.func-move .box',
+  translateX: function() {
+   console.log(prevTranslateX);
+    return prevTranslateX + anime.random(100, 200)
+  },
+  autoplay:false,
+  changeComplete: function(el) {
+    prevTranslateX = Number(el.animations[0].currentValue.slice(0,-2)
+  }
+})
+let funcBtn01 = document.getElementById('funcBtn01')
+funcBtn01.onclick = funcAnim01.play
